3fb6858146f97e09fd6d768feb85770f314efcd2
Mounesh Acharya <mounesh.acharya51@gmail.com>
2024-06-24 09:27:19 +0530

    changes the flyway script

--- src/main/java/com/flyway/sb_flyway_mysql/User.java
+++ src/main/java/com/flyway/sb_flyway_mysql/User.java
@@ -1,14 +1,19 @@
 package com.flyway.sb_flyway_mysql;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
 @Table(name = "user")
+@Data
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
+
+    @Column(name = "mob_num")
+    private Integer mobNum;
 
 }
